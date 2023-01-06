926ef85e298836902bc20b7fef1418a82ace7c9f
ax-ab <98651633+ax-lab@users.noreply.github.com>
2023-01-06 23:06:11 +1300

    add CLEAN and RELEASE flags to bootstrap
    - use environment variables to control the cargo build

--- byte.go
+++ byte.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -14,12 +15,41 @@ func main() {
 	bootstrap.Boot()
 
 	var cargoDir = filepath.Join(bootstrap.ProjectDir(), ByteCargoDir)
+
+	var (
+		clean   = os.Getenv("CLEAN") != ""
+		release = os.Getenv("RELEASE") != ""
+	)
+
+	if clean {
+		fmt.Printf("\nCleaning up source files...\n\n")
+	}
+
 	var success = bootstrap.ExecInDir("[cargo]", cargoDir, func() bool {
-		return bootstrap.Run("[cargo]", "cargo", "build", "--quiet")
+		if clean {
+			return bootstrap.Run("[cargo]", "cargo", "clean")
+		}
+		args := []string{"build", "--quiet"}
+		if release {
+			args = append(args, "--release")
+		}
+
+		return bootstrap.Run("[cargo]", "cargo", args...)
 	})
 
+	if clean {
+		return
+	}
+
 	if success {
-		var exePath = filepath.Join(cargoDir, "target", "debug", bootstrap.Exe("byte"))
-		bootstrap.Spawn(exePath, os.Args...)
+		var outputDir = "debug"
+		if release {
+			outputDir = "release"
+		}
+		var exePath = filepath.Join(cargoDir, "target", outputDir, bootstrap.Exe("byte"))
+		if err := bootstrap.Spawn(exePath, os.Args...); err != nil {
+			fmt.Fprintf(os.Stderr, "\n[bootstrap] error: starting byte: %v\n\n", err)
+			os.Exit(123)
+		}
 	}
 }
