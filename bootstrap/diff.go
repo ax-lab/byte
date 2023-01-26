86ea3b83ea9e9002ebb5740cc38e673f76a08f1d
ax-ab <98651633+ax-lab@users.noreply.github.com>
2023-01-26 21:27:23 +1300

    add myers diff algorithm basic implementation

--- bootstrap/diff.go
+++ bootstrap/diff.go
@@ -123,6 +123,10 @@ func computeDiff[T comparable](input, output []T) (out []DiffBlock) {
 }
 
 func computeLcs[T comparable](a, b []T) (out [][3]int) {
+	return LCS(a, 0, len(a), b, 0, len(b))
+}
+
+func ComputeLcsOld[T comparable](a, b []T) (out [][3]int) {
 	m := make([]int, len(a)*len(b))
 
 	get := func(xa, xb int) int {
@@ -261,5 +265,186 @@ func ComputeD(a, b string) int {
 	}
 
 	// The above loop is garanteed to find a D-path as long as `MAX = N + M`.
+	panic("unreachable")
+}
+
+func LCS[T comparable](a []T, a0, a1 int, b []T, b0, b1 int) (out [][3]int) {
+	if a1-a0 <= 0 || b1-b0 <= 0 {
+		return nil
+	}
+
+	n := a1 - a0
+	m := b1 - b0
+	d, x, y, u, v := findMidSnake(a[a0:a1], b[b0:b1])
+	x += a0
+	y += b0
+	u += a0
+	v += b0
+	if d > 1 {
+		out = LCS(a, a0, u, b, b0, v)
+		if x-u > 0 {
+			out = append(out, [3]int{u, v, x - u})
+		}
+		out = append(out, LCS(a, x, a1, b, y, b1)...)
+	} else {
+		if n < m {
+			if a[0] == b[0] {
+				out = append(out, [3]int{a0, b0, n})
+			} else {
+				out = append(out, [3]int{a0, b0 + 1, n})
+			}
+		} else {
+			if a[0] == b[0] {
+				out = append(out, [3]int{a0, b0, m})
+			} else {
+				out = append(out, [3]int{a0 + 1, b0, m})
+			}
+		}
+	}
+
+	return out
+}
+
+// Given Myers diff-algorithm, this function will find the `D` value for the
+// optimal D-path and also the `(x,y) -> (u,v)` coordinates for the middle
+// diagonal "snake" in the path (which can be potentially empty).
+//
+// Given that information, one can recursively construct the optimal edit
+// path (i.e. find the LCS of both sequences), which contains that diagonal.
+//
+// Formally, the middle snake is a diagonal in the optimal edit path that
+// decomposes the optimal D-path from (0,0) to (N,M) into:
+//
+// - ⌈D/2⌉ path from (0,0) to (x,y)
+// - ⌊D/2⌋ path from (u,v) to (N,M)
+//
+// Where `x - y = u - v` and `x >= u`. That is, the diagonal contains the
+// overlap of both paths.
+//
+// Additionally, `u + v >= ⌈D/2⌉` and `x + y <= N + M - ⌊D/2⌋`.
+func findMidSnake[T comparable](a, b []T) (d int, x, y, u, v int) {
+	// The idea of this algorithm is to run the basic D-path finding procedure
+	// in both forward and reverse directions until an overlap is found.
+	//
+	// When this overlap is found, we have found an optimal path. Furthermore,
+	// we have access to its middle diagonal.
+	//
+	// Note that this algorithm relies heavily on parity, given that:
+	//
+	// >  par(D) = par(K) = par(N - M)
+	//
+	// Also note that `-D <= K <= D`.
+
+	// This is the maximum value of D, when A and B have no common element
+	// and edit path removes every element from A and adds B.
+	n, m := len(a), len(b)
+	max_d := n + m
+
+	// But since we are looking in both directions simutaneously we only need
+	// to look for half D values half-way.
+	half_d := (n + m + 1) / 2
+
+	// This parity defines ⌈D/2⌉ and ⌊D/2⌋ which determines if we need to
+	// look for an overlap on the forward or reverse D-path searches.
+	//
+	// If even then ⌈D/2⌉ == ⌊D/2⌋, so we look for the overlap on the reverse
+	// search of the D/2 iteration. Otherwise ⌈D/2⌉ == ⌊D/2⌋+1 and we look for
+	// an overlap on the forward search of the ⌈D/2⌉ iteration.
+	delta := n - m
+	odd := delta%2 != 0
+
+	// keep one vector for the forward and reverse paths
+	vec_f := make([]int, 2*max_d+1)
+	vec_b := make([]int, 2*max_d+1)
+	for i := 0; i < len(vec_b); i++ {
+		vec_b[i] = n
+	}
+
+	idx := func(i int) int {
+		return i + max_d
+	}
+	get_f := func(i int) int {
+		return vec_f[idx(i)]
+	}
+	set_f := func(i, val int) {
+		vec_f[idx(i)] = val
+	}
+
+	get_r := func(i int) int {
+		return vec_b[idx(i)]
+	}
+	set_r := func(i, val int) {
+		vec_b[idx(i)] = val
+	}
+
+	// Look for the furthest reaching path for increasing values of D-path in
+	// both directions until an overlap is found.
+	for hd := 0; hd <= half_d; hd++ {
+		// First going forward. This is the same as the basic algorithm to
+		// find a D-path.
+		for k := -hd; k <= hd; k += 2 {
+			// Build the furthest reaching D-path for the current diagonal K
+			// by extending the the furthest reaching (D-1) path computed in
+			// the previous iteration
+			if k == -hd || (k != hd && get_f(k-1) < get_f(k+1)) {
+				x = get_f(k + 1) // extend vertical edge from diag K+1
+			} else {
+				x = get_f(k-1) + 1 // extend horizontal edge from diag K-1
+			}
+
+			// extend the diagonal "snake" to find the furthest reaching point
+			y = x - k
+			for x < len(a) && y < len(b) && a[x] == b[y] {
+				x++
+				y++
+			}
+
+			// store the furthest reaching point
+			set_f(k, x)
+
+			// Check the overlap condition with the previous reverse iteration.
+			if odd && k >= delta-(hd-1) && k <= delta+(hd-1) {
+				u = get_r(k)
+				v = u - k
+				if u <= x {
+					d = 2*hd - 1
+					return
+				}
+			}
+		}
+
+		// furthest reaching reverse path in diagonal k+delta
+		for k := hd; k >= -hd; k -= 2 {
+			kd := k + delta
+			// Build the furthest reaching D-path for the current diagonal K
+			// by extending the the furthest reaching (D-1) path computed in
+			// the previous iteration
+			if k == hd || (k != -hd && get_r(kd-1) < get_r(kd+1)) {
+				u = get_r(kd - 1)
+			} else {
+				u = get_r(kd+1) - 1
+			}
+
+			// extend the diagonal "snake" to find the furthest reaching point
+			v = u - kd
+			for u > 0 && v > 0 && a[u-1] == b[v-1] {
+				u--
+				v--
+			}
+
+			// store the furthest reaching point
+			set_r(kd, u)
+
+			if !odd && kd >= -hd && kd <= hd {
+				x = get_f(kd)
+				y = x - kd
+				if u <= x {
+					d = 2 * hd
+					return
+				}
+			}
+		}
+	}
+
 	panic("unreachable")
 }
