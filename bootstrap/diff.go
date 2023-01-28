357c8909b7993dce4df60b9800bec814f5a05775
ax-ab <98651633+ax-lab@users.noreply.github.com>
2023-01-28 20:04:17 +1300

    improve myers diff implementation
    - add version returning all possible snakes and cleaned up code
    - improve algorithm explanation

--- bootstrap/diff.go
+++ bootstrap/diff.go
@@ -275,11 +275,20 @@ func LCS[T comparable](a []T, a0, a1 int, b []T, b0, b1 int) (out [][3]int) {
 
 	n := a1 - a0
 	m := b1 - b0
-	d, x, y, u, v := findMidSnake(a[a0:a1], b[b0:b1])
+
+	ls := diffFindMidSnakes(a[a0:a1], b[b0:b1])
+	it := ls[0]
+	d := it.Diff
+	u := it.PosA
+	v := it.PosB
+	x := u + it.Len
+	y := v + it.Len
+
 	x += a0
 	y += b0
 	u += a0
 	v += b0
+
 	if d > 1 {
 		out = LCS(a, a0, u, b, b0, v)
 		if x-u > 0 {
@@ -305,146 +314,293 @@ func LCS[T comparable](a []T, a0, a1 int, b []T, b0, b1 int) (out [][3]int) {
 	return out
 }
 
-// Given Myers diff-algorithm, this function will find the `D` value for the
-// optimal D-path and also the `(x,y) -> (u,v)` coordinates for the middle
-// diagonal "snake" in the path (which can be potentially empty).
+type diffSnake struct {
+	PosA, PosB int // Coordinates for the diagonal (offset in A and B)
+	Len        int // Length of the diagonal
+	Diff       int // Edit count for the containing edit path (D for the optimal D-path)
+}
+
+// Find the middle snakes for the optimal edit D-paths between A and B for
+// the Myers diff algorithm.
 //
-// Given that information, one can recursively construct the optimal edit
-// path (i.e. find the LCS of both sequences), which contains that diagonal.
+// This will return a list of snakes for the optimal D-paths and the value
+// for D. Each snake is a (possibly empty) diagonal in the edit path which
+// corresponds to a common subsequence between A and B.
 //
-// Formally, the middle snake is a diagonal in the optimal edit path that
-// decomposes the optimal D-path from (0,0) to (N,M) into:
+// More importantly, a snake divides the D-path into ⌈D/2⌉ and ⌊D/2⌋ paths
+// which can be used to efficiently build an optimal edit script, that is,
+// the LCS between A abd B, by splitting the search.
 //
-// - ⌈D/2⌉ path from (0,0) to (x,y)
-// - ⌊D/2⌋ path from (u,v) to (N,M)
+// Formally, the middle snake for a D-path from (0,0) to (N,M) is a diagonal
+// in the edit grid with coordinates (x,y) -> (u,v) that splits the path as:
 //
-// Where `x - y = u - v` and `x >= u`. That is, the diagonal contains the
+// - a ⌈D/2⌉ path from (0,0) to (x,y)
+// - a ⌊D/2⌋ path from (u,v) to (N,M)
+//
+// Where `x-y = u-v` and `x >= u`. That is, the diagonal contains the
 // overlap of both paths.
 //
-// Additionally, `u + v >= ⌈D/2⌉` and `x + y <= N + M - ⌊D/2⌋`.
-func findMidSnake[T comparable](a, b []T) (d int, x, y, u, v int) {
-	// The idea of this algorithm is to run the basic D-path finding procedure
-	// in both forward and reverse directions until an overlap is found.
-	//
-	// When this overlap is found, we have found an optimal path. Furthermore,
-	// we have access to its middle diagonal.
-	//
-	// Note that this algorithm relies heavily on parity, given that:
-	//
-	// >  par(D) = par(K) = par(N - M)
-	//
-	// Also note that `-D <= K <= D`.
+// Additionally, `u+v >= ⌈D/2⌉` and `x+y <= N+M - ⌊D/2⌋`.
+//
+// Also note that for the returned `diffSnake` values:
+//
+// > u = PosA, v = PosB, x = PosA + Len, y = PosB + Len, d = Diff
+func diffFindMidSnakes[T comparable](a, b []T) (out []diffSnake) {
 
-	// This is the maximum value of D, when A and B have no common element
-	// and edit path removes every element from A and adds B.
+	//------------------------------------------------------------------------//
+	//
+	// Edit grids, D-paths, and snakes
+	// ===============================
+	//
+	// Below is a sample edit grid. Any path in this grid describes a set of
+	// edit operations that when applied transform A into B:
+	//
+	//   - horz edges from col X delete A[x]
+	//   - vert edges from row Y insert B[y]
+	//   - diagonal edges mean no edit, that is A[x] = B[y]
+	//
+	//         A[N=7] = abcabba
+	//         B[M=6] = cbabac
+	//
+	//         a b c a b b a
+	//       c ┌─┬─o─┬─┬─┬─┬─┐ 0
+	//         │ │ │╲│ │ │ │ │
+	//       b ┝─o─┼─┼─o─o─┼─┤ 1
+	//         │ │╲│ │ │╲│╲│ │
+	//       a o─┼─┼─o─┼─┼─o─┤ 2
+	//         │╲│ │ │╲│ │ │╲│
+	//       b ┝─o─┼─┼─o─o─┼─┤ 3
+	//         │ │╲│ │ │╲│╲│ │
+	//       a o─┼─┼─o─┼─┼─o─┤ 4
+	//         │╲│ │ │╲│ │ │╲│
+	//       c ┝─┼─o─┼─┼─┼─┼─┤ 5
+	//         │ │ │╲│ │ │ │ │
+	//         ┕─┴─┴─┴─┴─┴─┴─┙ 6
+	//         0 1 2 3 4 5 6 7
+	//
+	// Diagonals in the grid correspond to common subsequences between A and B,
+	// being referred here as "snakes".
+	//
+	// For any path in the above grid, we can define D as its number of non-
+	// diagonal edges, which in turn corresponds to the number of individual
+	// edit operations. Any path from (0,0) to (N,M) will edit A into B.
+	//
+	// Conversely, a D-path is any path in the grid with exactly D horizontal
+	// or diagonal edges.
+	//
+	// The goal is to maximize the diagonal edges in a path, minimizing D and
+	// the number of edit operations. This is the same problem as finding the
+	// Longest Common Subsequence (LCS).
+	//
+	//
+	// Diagonals and K
+	// ===============
+	//
+	// We can also refer to a diagonal K in the grid, where `K = X-Y`.
+	//
+	//        0    1   2   3   4   5   6    7
+	//         ┌───┬───┬───┬───┬───┬───┬───┐
+	//         │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │
+	//       -1┝───┼───┼───┼───┼───┼───┼───┤6
+	//         │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │
+	//       -2┝───┼───┼───┼───┼───┼───┼───┤5
+	//         │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │
+	//       -3┝───┼───┼───┼───┼───┼───┼───┤4
+	//         │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │
+	//       -4┝───┼───┼───┼───┼───┼───┼───┤3
+	//         │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │
+	//       -5┝───┼───┼───┼───┼───┼───┼───┤2
+	//         │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │ ╲ │
+	//         ┕───┴───┴───┴───┴───┴───┴───┙
+	//       -6   -5  -4  -3  -2  -1   0    1
+	//
+	//
+	// Additional definitions
+	// ======================
+	//
+	// Following are important definitions that form the basis of this
+	// algorithm.
+	//
+	// About D-paths and the diagonals K in the grid:
+	//
+	//   1) Any D-path will always end in a diagonal K where -D ≤ K ≤ D.
+	//   2) The odd-parity of D and K is always the same.
+	//
+	// Also important to note that D ≤ len(A)+len(B).
+	//
+	// Additionally:
+	//
+	//   3) A D-path can always be decomposed into a (D-1)-path followed by
+	//      a non-diagonal edge and a possibly empty diagonal "snake".
+	//
+	//
+	// Furthest reaching D-paths
+	// =========================
+	//
+	// A furthest reaching D-path is one that ends as far away as possible
+	// from the (0,0) grid origin.
+	//
+	// The furthest reaching D-path for a diagonal K can be generated by
+	// extending the furthest reaching (D-1) path from K+1 and K-1 and the
+	// longest possible diagonal "snake".
+	//
+	//
+	// Myers algorithm
+	// ==============
+	//
+	// This algorithm works by finding the furthest reaching D-paths for
+	// successive values of D for each of the K diagonals.
+	//
+	// The code works by searching for both forward and reverse paths until
+	// paths with an overlapping diagonal "snake" are found. The snake is a
+	// common subsequence of A and B and part of an optimal D-path.
+	//
+	// Moreover, the snake splits the optimal D-path into ⌈D/2⌉ and ⌊D/2⌋
+	// paths. This can be used to recursively build the edit script, that is,
+	// the LCS between A and B.
+	//
+	// Note that the overlapping snake can a diagonal of zero length.
+	//
+	// For each diagonal K, the furthest reaching point for the current D
+	// is recorded in an array indexed by K. Since K = X-Y, only the value
+	// of X needs to be stored.
+	//
+	// Since K and D have the same parity, for each D the algorithm computes
+	// only the respective odd/even diagonals. This also means that the values
+	// for K are disjoint from K+1 and K-1, allowing iterations to depend on
+	// the previously computed D-1 values.
+	//
+	//------------------------------------------------------------------------//
+
 	n, m := len(a), len(b)
-	max_d := n + m
+	maxD := n + m
 
-	// But since we are looking in both directions simutaneously we only need
-	// to look for half D values half-way.
-	half_d := (n + m + 1) / 2
+	// The maximum possible D is n+m, but since we work from both ends the loop
+	// only needs to search until ⌈D/2⌉.
+	halfD := (n + m + 1) / 2
 
-	// This parity defines ⌈D/2⌉ and ⌊D/2⌋ which determines if we need to
-	// look for an overlap on the forward or reverse D-path searches.
-	//
-	// If even then ⌈D/2⌉ == ⌊D/2⌋, so we look for the overlap on the reverse
-	// search of the D/2 iteration. Otherwise ⌈D/2⌉ == ⌊D/2⌋+1 and we look for
-	// an overlap on the forward search of the ⌈D/2⌉ iteration.
+	// The main diagonal for the reverse paths is delta. This also affects the
+	// even/odd parity of the D-path.
 	delta := n - m
+
+	// The D for the edit path will always have the same parity as the size
+	// difference between A and B (we need inserts or deletes to make up for
+	// the size difference, while swapping elements preserves parity).
+	//
+	// This means that the odd/even parity of delta determines if a path
+	// overlap is possible in the forward or reverse loops (see below).
 	odd := delta%2 != 0
 
-	// keep one vector for the forward and reverse paths
-	vec_f := make([]int, 2*max_d+1)
-	vec_b := make([]int, 2*max_d+1)
-	for i := 0; i < len(vec_b); i++ {
-		vec_b[i] = n
+	// Furthest reaching D-path for diagonal K, where -maxD ≤ K ≤ maxD
+	// for the forward and reverse paths.
+	//
+	// Note that since K=X-Y, we store only X with Y=X-K.
+	fwd := make([]int, 2*maxD+1)
+	rev := make([]int, len(fwd))
+
+	// For the reverse path, all diagonals start at the right of the grid.
+	for i := 0; i < len(rev); i++ {
+		rev[i] = n
 	}
 
-	idx := func(i int) int {
-		return i + max_d
-	}
-	get_f := func(i int) int {
-		return vec_f[idx(i)]
-	}
-	set_f := func(i, val int) {
-		vec_f[idx(i)] = val
-	}
+	// Offset to apply when indexing fwd and rev, since K can be negative.
+	off := maxD
 
-	get_r := func(i int) int {
-		return vec_b[idx(i)]
-	}
-	set_r := func(i, val int) {
-		vec_b[idx(i)] = val
-	}
+	// Search the furthest forward and reverse reaching paths for increasing
+	// values of (half) D until we find an overlapping snake between them.
+	for hd := 0; len(out) == 0 && hd <= halfD; hd++ {
+		// Note that the following applies for these inner loops:
+		//
+		// - Forward path: D-path is HD + (HD - 1)
+		// - Reverse path: D-path is HD + HD
+		//
+		// The forward path is always building towards an odd D-path, while
+		// the reverse is always even.
+		//
+		// Since D will always have the same parity as `delta`, we only bother
+		// checking for overlaps when the parity is possible.
 
-	// Look for the furthest reaching path for increasing values of D-path in
-	// both directions until an overlap is found.
-	for hd := 0; hd <= half_d; hd++ {
-		// First going forward. This is the same as the basic algorithm to
-		// find a D-path.
+		// forward path
 		for k := -hd; k <= hd; k += 2 {
-			// Build the furthest reaching D-path for the current diagonal K
-			// by extending the the furthest reaching (D-1) path computed in
-			// the previous iteration
-			if k == -hd || (k != hd && get_f(k-1) < get_f(k+1)) {
-				x = get_f(k + 1) // extend vertical edge from diag K+1
+			// Extend the furthest X from either K-1 or K+1.
+			var x int
+			if k == -hd || (k != hd && fwd[k-1+off] < fwd[k+1+off]) {
+				// vert edge from K+1
+				x = fwd[k+1+off]
 			} else {
-				x = get_f(k-1) + 1 // extend horizontal edge from diag K-1
+				// horz edge from K-1 (preferred case since it increases x)
+				x = fwd[k-1+off] + 1
 			}
 
-			// extend the diagonal "snake" to find the furthest reaching point
-			y = x - k
+			// Extend the diagonal.
+			y := x - k
 			for x < len(a) && y < len(b) && a[x] == b[y] {
 				x++
 				y++
 			}
 
-			// store the furthest reaching point
-			set_f(k, x)
+			fwd[k+off] = x
 
-			// Check the overlap condition with the previous reverse iteration.
+			// Check for overlap with the reverse (D-1)-paths (note that
+			// the central reverse diagonal is delta).
 			if odd && k >= delta-(hd-1) && k <= delta+(hd-1) {
-				u = get_r(k)
-				v = u - k
-				if u <= x {
-					d = 2*hd - 1
-					return
+				if u := rev[k+off]; u <= x {
+					out = append(out, diffSnake{
+						PosA: u,
+						PosB: u - k, // v
+						Len:  x - u,
+						Diff: 2*hd - 1,
+					})
 				}
 			}
 		}
 
-		// furthest reaching reverse path in diagonal k+delta
+		// reverse path
 		for k := hd; k >= -hd; k -= 2 {
-			kd := k + delta
-			// Build the furthest reaching D-path for the current diagonal K
-			// by extending the the furthest reaching (D-1) path computed in
-			// the previous iteration
-			if k == hd || (k != -hd && get_r(kd-1) < get_r(kd+1)) {
-				u = get_r(kd - 1)
+			// the reverse K diagonal
+			kr := k + delta
+
+			// Extend the furthest U from either K-1 or K+1.
+			//
+			// Note the following differences from above since we are on the
+			// reverse path and moving top left on the grid:
+			//
+			// - K=0 should be the corner diagonal, so K needs the delta
+			//   applied when in reverse;
+			// - When K is on the edge (-D or +D) the behaviour is different;
+			// - Horizontal edges extend from K+1 and vertical from K-1;
+			// - We want to minimize the horizontal position.
+			var u int
+			if k == hd || (k != -hd && rev[kr+1+off] > rev[kr-1+off]) {
+				// vert edge from K-1
+				u = rev[kr-1+off]
 			} else {
-				u = get_r(kd+1) - 1
+				// horz edge from K+1
+				u = rev[kr+1+off] - 1
 			}
 
-			// extend the diagonal "snake" to find the furthest reaching point
-			v = u - kd
+			// Extend the diagonal.
+			v := u - kr
 			for u > 0 && v > 0 && a[u-1] == b[v-1] {
 				u--
 				v--
 			}
 
-			// store the furthest reaching point
-			set_r(kd, u)
+			rev[kr+off] = u
 
-			if !odd && kd >= -hd && kd <= hd {
-				x = get_f(kd)
-				y = x - kd
-				if u <= x {
-					d = 2 * hd
-					return
+			// Check for overlap with the forward D-paths computed above.
+			if !odd && kr >= -hd && kr <= hd {
+				if x := fwd[kr+off]; x >= u {
+					out = append(out, diffSnake{
+						PosA: u,
+						PosB: v,
+						Len:  x - u,
+						Diff: 2 * hd,
+					})
 				}
 			}
 		}
 	}
 
-	panic("unreachable")
+	return
 }
