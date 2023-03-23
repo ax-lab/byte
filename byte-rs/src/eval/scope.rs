ee3ca009766a38974a1e34f40914e50114dd73ec
ax-ab <98651633+ax-lab@users.noreply.github.com>
2023-03-23 20:09:31 +1300

    flesh out new scope protocol

--- byte-rs/src/eval/scope.rs
+++ byte-rs/src/eval/scope.rs
@@ -1,17 +1,31 @@
-use crate::lexer::{Lex, Stream};
+use crate::{
+	lexer::{Lex, Stream, Token},
+	Error,
+};
 
 pub enum Action {
 	None,
-	Complete,
 	EnterChild {
 		scope: Box<dyn Scope>,
 		isolated: bool,
 	},
 }
 
+pub enum Filter {
+	No,
+	Skip,
+	Stop { skip: bool },
+}
+
+pub enum Stop<'a> {
+	Ok,
+	Error(Error<'a>),
+}
+
 pub trait Scope {
 	fn check_action(&mut self, input: &mut Stream) -> Action;
-	fn should_skip(&mut self, input: &Stream) -> bool;
+	fn check_filter(&mut self, input: &Stream) -> Filter;
+	fn leave<'a>(&self, input: &Stream<'a>) -> Stop<'a>;
 }
 
 pub enum Scoped {
@@ -23,6 +37,12 @@ pub enum Scoped {
 	},
 }
 
+#[derive(Clone, Debug)]
+pub enum LexResult<'a> {
+	Ok(Lex<'a>),
+	Error(Error<'a>),
+}
+
 impl Default for Scoped {
 	fn default() -> Self {
 		Scoped::Root
@@ -30,24 +50,14 @@ impl Default for Scoped {
 }
 
 impl Scoped {
-	pub fn next<'a>(&mut self, input: &mut Stream<'a>) -> Lex<'a> {
+	pub fn next<'a>(&mut self, input: &mut Stream<'a>) -> LexResult<'a> {
 		loop {
-			// a child scope will only operate on its parent output
-			if let Scoped::Child { parent, .. } = self {
-				if let Some(parent) = parent {
-					if parent.should_skip(input) {
-						input.next();
-						continue;
-					}
-				}
-			}
-
 			match self {
 				// the root scope just reads the next value without filtering
 				Scoped::Root => {
 					let next = input.value();
 					input.next();
-					return next;
+					return LexResult::Ok(next);
 				}
 
 				Scoped::Child {
@@ -55,32 +65,48 @@ impl Scoped {
 					scope,
 					parent,
 				} => {
-					// non-isolated child scopes respect the parent skipping
+					// non-isolated child scopes defer to the parent first
+					let mut stopped = false;
 					if !*isolated {
 						if let Some(parent) = parent {
-							if parent.should_skip(input) {
-								input.next();
-								continue;
+							match parent.check_filter(input) {
+								Filter::No => {}
+								Filter::Skip => {
+									input.next();
+									continue;
+								}
+								Filter::Stop { skip } => {
+									if skip {
+										input.next();
+									}
+									stopped = true;
+								}
 							}
 						}
 					}
+
+					stopped = stopped || input.value().is_none();
 
 					// check the current scope action
 					match scope.check_action(input) {
 						Action::None => {
 							let next = input.value();
-							let skip = scope.should_skip(&input);
-							input.next();
-							if !skip {
-								return next;
-							}
-						}
-						Action::Complete => {
-							if let Some(parent) = parent {
-								let parent = std::mem::take(&mut **parent);
-								std::mem::replace(self, parent);
-							} else {
-								return input.value().as_none();
+							let skip = match scope.check_filter(input) {
+								Filter::No => false,
+								Filter::Skip => true,
+								Filter::Stop { skip } => {
+									stopped = false;
+									if skip {
+										input.next();
+									}
+									false
+								}
+							};
+							if !stopped {
+								input.next();
+								if !skip {
+									return LexResult::Ok(next);
+								}
 							}
 						}
 						Action::EnterChild { scope, isolated } => {
@@ -89,7 +115,20 @@ impl Scoped {
 								isolated,
 								scope,
 								parent: Some(current.into()),
-							}
+							};
+							continue;
+						}
+					}
+
+					if stopped {
+						if let Stop::Error(error) = scope.leave(input) {
+							return LexResult::Error(error);
+						}
+						if let Some(parent) = parent {
+							let parent = std::mem::take(&mut **parent);
+							std::mem::replace(self, parent);
+						} else {
+							return LexResult::Ok(input.value().as_none());
 						}
 					}
 				}
@@ -97,25 +136,126 @@ impl Scoped {
 		}
 	}
 
-	fn should_skip(&mut self, input: &Stream) -> bool {
+	fn check_filter(&mut self, input: &Stream) -> Filter {
 		match self {
-			Scoped::Root => false,
+			Scoped::Root => Filter::No,
 			Scoped::Child {
 				isolated,
 				scope,
 				parent,
 			} => {
-				let should_skip = if *isolated {
-					false
-				} else {
+				if !*isolated {
 					if let Some(parent) = parent {
-						parent.should_skip(input)
-					} else {
-						false
+						match parent.check_filter(input) {
+							Filter::No => {}
+							filter @ (Filter::Skip | Filter::Stop { .. }) => return filter,
+						}
 					}
-				};
-				should_skip || scope.should_skip(input)
+				}
+				scope.check_filter(input)
 			}
 		}
+	}
+}
+
+struct ScopeIndented {
+	level: usize,
+}
+
+impl ScopeIndented {
+	fn new() -> Box<dyn Scope> {
+		Box::new(ScopeIndented { level: 0 })
+	}
+}
+
+impl Scope for ScopeIndented {
+	fn check_action(&mut self, input: &mut Stream) -> Action {
+		Action::None
+	}
+
+	fn check_filter(&mut self, input: &Stream) -> Filter {
+		match input.token() {
+			Token::Indent => {
+				self.level += 1;
+				if self.level == 1 {
+					Filter::Skip // skip first indent
+				} else {
+					Filter::No
+				}
+			}
+			Token::Break => {
+				if self.level == 1 {
+					let mut input = input.clone();
+					input.next();
+					if input.token() == Token::Dedent {
+						Filter::Skip // strip break before last dedent
+					} else {
+						Filter::No
+					}
+				} else {
+					Filter::No
+				}
+			}
+			Token::Dedent => {
+				self.level -= 1;
+				if self.level == 0 {
+					Filter::Stop { skip: true }
+				} else {
+					Filter::No
+				}
+			}
+			_ => Filter::No,
+		}
+	}
+
+	fn leave<'a>(&self, input: &Stream<'a>) -> Stop<'a> {
+		if self.level > 0 {
+			panic!(
+				"lexer generated unbalanced indentation for {}",
+				input.span()
+			)
+		}
+		Stop::Ok
+	}
+}
+
+struct ScopeLine {
+	ended: bool,
+}
+
+impl ScopeLine {
+	fn new() -> Box<dyn Scope> {
+		Box::new(ScopeLine { ended: false })
+	}
+}
+
+impl Scope for ScopeLine {
+	fn check_action(&mut self, input: &mut Stream) -> Action {
+		if input.token() == Token::Break {
+			input.next();
+			if input.token() == Token::Indent {
+				Action::EnterChild {
+					scope: ScopeIndented::new(),
+					isolated: true,
+				}
+			} else {
+				self.ended = true;
+				Action::None
+			}
+		} else {
+			Action::None
+		}
+	}
+
+	fn check_filter(&mut self, input: &Stream) -> Filter {
+		if self.ended {
+			Filter::Stop { skip: false }
+		} else {
+			Filter::No
+		}
+	}
+
+	fn leave<'a>(&self, input: &Stream<'a>) -> Stop<'a> {
+		Stop::Ok
 	}
 }
