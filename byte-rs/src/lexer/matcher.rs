0f9fe1b656ae650167e9e66ade3166158ef49cd1
ax-ab <98651633+ax-lab@users.noreply.github.com>
2023-05-20 14:52:47 +1200

    remove unused code

--- byte-rs/src/lexer/matcher.rs
+++
@@ -1,9 +0,0 @@
-use crate::core::error::*;
-use crate::core::input::*;
-
-use super::*;
-
-/// Trait used by the [`Lexer`] to match tokens.
-pub trait Matcher: Send + Sync {
-	fn try_match(&self, next: char, input: &mut Cursor, errors: &mut ErrorList) -> Option<Token>;
-}
