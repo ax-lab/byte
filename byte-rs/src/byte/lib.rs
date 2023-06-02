80f2d59e7c9b357c18350fe14134a3bdcfd4c37b
ax-ab <98651633+ax-lab@users.noreply.github.com>
2023-06-02 14:39:55 +1200

    move errors to util

--- byte-rs/src/byte/lib.rs
+++ byte-rs/src/byte/lib.rs
@@ -1,7 +1,3 @@
-pub mod errors;
 pub mod util;
 
-pub use errors::*;
 pub use util::*;
-
-use std::fmt::Write;
--- byte-rs/src/byte/util.rs
+++ byte-rs/src/byte/util.rs
@@ -1,9 +1,13 @@
 pub mod common;
+pub mod errors;
 pub mod format;
 pub mod traits;
 pub mod value;
 
 pub use common::*;
+pub use errors::*;
 pub use format::*;
 pub use traits::*;
 pub use value::*;
+
+use std::fmt::Write;
