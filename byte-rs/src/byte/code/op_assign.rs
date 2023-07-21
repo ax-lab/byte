442ce2b908d871a70065eb242d131e97c13e8722
ax-ab <98651633+ax-lab@users.noreply.github.com>
2023-07-21 18:46:29 +1200

    fix error span in op assign

--- byte-rs/src/byte/code/op_assign.rs
+++ byte-rs/src/byte/code/op_assign.rs
@@ -6,18 +6,17 @@ pub struct OpAssign(pub Type);
 impl IsBinaryOp for OpAssign {
 	fn execute(&self, scope: &mut RuntimeScope, lhs: &Expr, rhs: &Expr) -> Result<ExprValue> {
 		let typ = rhs.get_type()?;
-		let rhs = rhs.execute(scope)?;
-		let lhs = lhs.execute(scope)?;
-		match lhs {
+		let rhs_val = rhs.execute(scope)?;
+		let lhs_val = lhs.execute(scope)?;
+		match lhs_val {
 			ExprValue::Value(..) => {
-				// TODO: runtime should have access to the source of expressions
 				let error = format!("cannot assign `{typ}` to non-reference");
-				let error = Errors::from(error, Span::default());
+				let error = Errors::from(error, lhs.span().clone());
 				Err(error)
 			}
 			ExprValue::Variable(name, offset, ..) => {
-				scope.set(name.clone(), offset, rhs.value().clone());
-				Ok(ExprValue::Variable(name, offset, rhs.into_value()))
+				scope.set(name.clone(), offset, rhs_val.value().clone());
+				Ok(ExprValue::Variable(name, offset, rhs_val.into_value()))
 			}
 		}
 	}
